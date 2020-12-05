ff6c6027223d5b3cc2481e38d214b2e51727cd04
Hank <horicky7@gmail.com>
2020-12-05 10:01:05 -0800

    index

--- fp/index.php
+++ fp/index.php
@@ -1,21 +1,158 @@
 <?php include './Includes/fp-config.php' ?>
 <?php include './Includes/header.php'; ?>
 
+<section id="picture">
+    <div id="pictureWrapper">
+        <img class="pic-1" src="./images/pic1.jpeg">
+        <img class="pic-2" src="./images/pic2.jpeg">
+        <img class="pic-3" src="./images/pic3.jpeg">
+    </div>
+    <div id="consulationWrapper">
+        <div id="consulationInfo">
+            <p>EFFICIENT</p>
+            <p>ECONOMIC</p>
+            <p>PROFESSIONAL</p>
+            <p class="outstanding">IN SEATTLE & PORTLAND</p>
+            <P><button>GET FREE CONSULATION NOW!</button></P>
+        </div>
+    </div>
+</section>
+<section id="services">
+    <div id="services-title">WHAT WE CAN DO FOR YOU? </div>
+    <div id="services-contents">
+        <div class="content-detail">
+            <div class="detail-wrapper">
+                <div class="detail-logo">
+                    <div class="logo-wrapper">
+                        <span class="iconfont icon-customization"></span>
+                    </div>
+                </div>
+                <div class="detail-description">
+                    <p class="detail-title">INTERRIOR DESGIN</p>
+                    <p class="detail-content">Interior designers make indoor spaces functional, safe, and beautiful
+                        by
+                        determining space requirements ... </p>
+                </div>
+            </div>
+        </div>
+        <div class="content-detail">
+            <div class="detail-wrapper">
+                <div class="detail-logo">
+                    <div class="logo-wrapper">
+                        <span class="iconfont icon-electronics"></span>
+                    </div>
+                </div>
+                <div class="detail-description">
+                    <p class="detail-title">MATERIAL RECOMMENDATION</p>
+                    <p class="detail-content">Apart from naturally occurring materials, many man-made products are
+                        in use, some more and some less synthetic .... </p>
+                </div>
+            </div>
+        </div>
+        <div class="content-detail">
+            <div class="detail-wrapper">
+                <div class="detail-logo">
+                    <div class="logo-wrapper">
+                        <span class="iconfont icon-dollar"></span>
+                    </div>
+                </div>
+                <div class="detail-description">
+                    <p class="detail-title">FREE CONSULATION</p>
+                    <p class="detail-content">We provide free consulation. Don't hesitate to leave your contact
+                        information for us. We will contact your in 2-3 bussness days.</p>
+                </div>
+            </div>
+        </div>
+        <div class="content-detail">
+            <div class="detail-wrapper">
+                <div class="detail-logo">
+                    <div class="logo-wrapper">
+                        <span class="iconfont icon-company"></span>
+                    </div>
+                </div>
+                <div class="detail-description">
+                    <p class="detail-title">CONSTRUCTION MANAGEMENT</p>
+                    <p class="detail-content">We will purchase all of the material for the project and organize
+                        constructors to accomplish the project. </p>
+                </div>
+            </div>
+        </div>
+        <div class="content-detail">
+            <div class="detail-wrapper">
+                <div class="detail-logo">
+                    <div class="logo-wrapper">
+                        <span class="iconfont icon-atm-away"></span>
+                    </div>
+                </div>
+                <div class="detail-description">
+                    <p class="detail-title">CUSTOMER SERVICES</p>
+                    <p class="detail-content"> 24 hours to receive your phone calls, text messages to inform you of
+                        the status of the project.</p>
+                </div>
+            </div>
+        </div>
+        <div class="content-detail">
+            <div class="detail-wrapper">
+                <div class="detail-logo">
+                    <div class="logo-wrapper">
+                        <span class="iconfont icon-color"></span>
+                    </div>
+                </div>
+                <div class="detail-description">
+                    <p class="detail-title">
+                        MAINTENANCE SERVICE</p>
+                    <p class="detail-content">Three months maintenance service. If you are not satisfied with the
+                        project, we will make further improvement. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <section id="instructions">
     <div class="instruction-wrapper">
         <div class="instruction-title">
-            <span>Template Title</span>
+            <span>ABOUT US</span>
         </div>
         <div class="instruction-contents clearfloat">
             <div class="instruction-text">
-                <p> <span>L</span>
-                    orem ipsum dolor, sit amet consectetur adipisicing elit. Ad nemo quae culpa ex eaque dolore eveniet minima sint ducimus vero nam, minus libero et iste, aliquam sequi similique neque nisi!
+                <p> <span>W</span>olf Construction specializes in residential and commercial construction
+                    services
+                    in YourCity
+                    and surrounding areas. Our company has a reputation for providing high quality services at an
+                    affordable price.
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sint amet ea, porro debitis incidunt aspernatur, architecto placeat illo corporis voluptas? Distinctio laudantium suscipit quo placeat, dignissimos quibusdam velit alias!
+                    Whether you want to remodel your kitchen, build a new home, or develop a commercial property,
+                    YourName Construction has the experience and expertise to help you get the job done right.
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum rem quas aut. Quisquam illum quod consequuntur a recusandae iure, consectetur doloribus praesentium vero tempore laborum aperiam ullam nulla tempora vitae?
+                    To get started with your project, learn more about the different types of services we offer
+                    below.
+                    We will take the time to thoroughly understand your project requirements and create a plan that
+                    fits
+                    into your budget.
+                </p>
+            </div>
+            <div class="img-wrapper">
+                <img src="./images/pic5.jpg" />
+            </div>
+        </div>
+    </div>
+    <div class="instruction-wrapper">
+        <div class="instruction-title">
+            <span>New Construction</span>
+        </div>
+        <div class="instruction-contents clearfloat">
+            <div class="instruction-text">
+                <p> <span>I</span>f you have ever dreamed of custom designing your own home, we can turn that dream into reality.
+
+                    At YourName Construction, we have extensive experience building custom homes. We offer a complete solution that will guide you through every step of the process. From pre-construction to picking out interior paint, we’ll help you design the home of your dreams at an affordable price.
+                </p>
+                <p>
+                    Our process is designed to put you in control of your new home. You can provide your input on every detail or let us provide you with options and let you decide from there.
+                </p>
+                <p>
+                    If you are thinking about a custom build, get a free consultation with YourName construction. We can help you take the first steps towards creating the home you have always wanted.
                 </p>
             </div>
             <div class="img-wrapper">
@@ -25,43 +162,21 @@
     </div>
     <div class="instruction-wrapper">
         <div class="instruction-title">
-            <span>Template Title</span>
+            <span>Commercial</span>
         </div>
         <div class="instruction-contents clearfloat">
             <div class="instruction-text">
-                <p> <span>L</span>
-                    orem ipsum dolor, sit amet consectetur adipisicing elit. Ad nemo quae culpa ex eaque dolore eveniet minima sint ducimus vero nam, minus libero et iste, aliquam sequi similique neque nisi!
+                <p> <span>I</span>f you are looking for a commercial developer in YourCity with proven experience we can help. We can help you design your next office space, addition, or remodel your existing office or commercial structure.
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sint amet ea, porro debitis incidunt aspernatur, architecto placeat illo corporis voluptas? Distinctio laudantium suscipit quo placeat, dignissimos quibusdam velit alias!
+                    We have handled projects ranging from minor office renovations to full development of multi-unit commercial properties.
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum rem quas aut. Quisquam illum quod consequuntur a recusandae iure, consectetur doloribus praesentium vero tempore laborum aperiam ullam nulla tempora vitae?
+                    Our company starts by doing a free consultation to find out more about what your business needs are. We will then work as a team to create plans, form a budget, and start constructions progress. All of our projects have a client oriented approach. We will provide you with a single point of contact to handle your project from start to finish.
                 </p>
             </div>
             <div class="img-wrapper">
-                <img src="./images/pic4.jpg" />
-            </div>
-        </div>
-    </div>
-    <div class="instruction-wrapper">
-        <div class="instruction-title">
-            <span>Template Title</span>
-        </div>
-        <div class="instruction-contents clearfloat">
-            <div class="instruction-text">
-                <p> <span>L</span>
-                    orem ipsum dolor, sit amet consectetur adipisicing elit. Ad nemo quae culpa ex eaque dolore eveniet minima sint ducimus vero nam, minus libero et iste, aliquam sequi similique neque nisi!
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sint amet ea, porro debitis incidunt aspernatur, architecto placeat illo corporis voluptas? Distinctio laudantium suscipit quo placeat, dignissimos quibusdam velit alias!
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum rem quas aut. Quisquam illum quod consequuntur a recusandae iure, consectetur doloribus praesentium vero tempore laborum aperiam ullam nulla tempora vitae?
-                </p>
-            </div>
-            <div class="img-wrapper">
-                <img src="./images/pic4.jpg" />
+                <img src="./images/pic6.jpg" />
             </div>
         </div>
     </div>
